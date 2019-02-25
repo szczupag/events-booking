338900bef1a689d59c04c23aba82add3068932e7
szczupag <jadziutka@gmail.com>
2019-02-25 21:32:16 +0100

    users & relations between users and events

--- app.js
+++ app.js
@@ -3,21 +3,21 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const Event = require('./models/event');
+const User = require('./models/user');
 
 const app = express();
 
-const events = [];
-
 app.use(bodyParser.json());
-
 
 app.use(
     '/graphql',
     graphqlHttp({
         // ! - not null
         // [String!]! - not null list of not null strings
+        // passoword in User is nullable because we DO NOT return password in any case
         schema: buildSchema(`
             type Event {
                 _id: ID!
@@ -25,6 +25,17 @@ app.use(
                 description: String!
                 price: Float!
                 date: String!
+            }
+
+            type User {
+                _id: ID!
+                email: String!
+                password: String
+            }
+
+            input UserInput {
+                email: String!
+                password: String!
             }
 
             input EventInput {
@@ -40,6 +51,7 @@ app.use(
 
             type RootMutation {
                 createEvent(eventInput: EventInput): Event
+                createUser(userInput: UserInput): User
             }
 
             schema {
@@ -66,19 +78,54 @@ app.use(
                     title: args.eventInput.title,
                     description: args.eventInput.description,
                     price: +args.eventInput.price,
-                    date: new Date(args.eventInput.date)
+                    date: new Date(args.eventInput.date),
+                    creator: '5c74474780fa26953e68b35a'
                 });
+                let createdEvent;
                 // returning promise 
                 return event
                     .save() // write data to database
                     .then(result => {
-                        console.log(result);
-                        return { ...result._doc }; // + meta data
+                        createdEvent = { ...result._doc }; // + meta data
+                        return User.findById('5c74474780fa26953e68b35a');
+                    })
+                    .then(user => {
+                        if (!user) {
+                            throw new Error('User not found!');
+                        }
+                        user.createdEvents.push(event);
+                        return user.save();
+                    })
+                    .then(result => {
+                        return createdEvent;
                     })
                     .catch(err => {
                         console.log(err);
                         throw err;
                     })
+            },
+            createUser: args => {
+                return User
+                    .findOne({ email: args.userInput.email }) // will always return undefined or object, so we don't need 'catch' block
+                    .then(user => {
+                        if (user) {
+                            throw new Error('User exists already!');
+                        }
+                        return bcrypt.hash(args.userInput.password, 12);
+                    })
+                    .then(hashedPassword => {
+                        const user = new User({
+                            email: args.userInput.email,
+                            password: hashedPassword
+                        });
+                        return user.save();
+                    })
+                    .then(result => {
+                        return { ...result._doc, password: null }
+                    })
+                    .catch(err => {
+                        throw err;
+                    });
             }
         },
         graphiql: true
