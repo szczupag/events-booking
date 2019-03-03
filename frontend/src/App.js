4b573a1392a75ba288b948bdc2218016c8b28534
szczupag <jadziutka@gmail.com>
2019-03-03 14:39:07 +0100

    user interface

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+
+import AuthPage from './pages/Auth';
+import EventsPage from './pages/Events';
+import BookingsPage from './pages/Bookings';
+import MainNavigation from './components/Navigation/MainNavigation';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact />
+              <Route path="/auth" component={AuthPage} />
+              <Route path="/events" component={EventsPage} />
+              <Route path="/bookings" component={BookingsPage} />
+            </Switch>
+          </main>
+        </React.Fragment>
+
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
---
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './MainNavigation.css';
+
+const mainNavigation = props => {
+    return (
+        <header className="main-navigation">
+            <div className="main-navigation__logo">
+                <h1>EasyEvent</h1>
+            </div>
+            <div className="main-navigation__items">
+                <ul>
+                    <li><NavLink to="/auth">Authenticate</NavLink></li>
+                    <li><NavLink to="/events">Events</NavLink></li>
+                    <li><NavLink to="/bookings">bookings</NavLink></li>
+                </ul>
+            </div>
+        </header>
+    );
+};
+
+export default mainNavigation;
